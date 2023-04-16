f0864402675d9dbe7ca1c917ea9d8d955a1bc85e
Daniel Rojas <daniel.rojas.artiaga@gmail.com>
2023-04-15 19:47:27 -0600

    lesson 2, I write a function to test the definitions.

--- src/test/java/com/l20j45/StringUtilTest.java
+++ src/test/java/com/l20j45/StringUtilTest.java
@@ -5,14 +5,17 @@ package com.l20j45;
 public class StringUtilTest {
     public static void main(String[] args) {
 
-        String result = StringUtil.repeat("hola",3);
-        if(!result.equals("holaholahola"))
-            System.out.println("Error");
+        assertEquals(StringUtil.repeat("hola",3),"holaholahola");
 
 
-        String result2 = StringUtil.repeat("hola",1);
-        if(!result2.equals("hola"))
-            System.out.println("Error");
+        assertEquals(StringUtil.repeat("hola",1),"hola");
+
+    }
+
+    private static void assertEquals(String actual, String esperado) {
+        if(!actual.equals(esperado)){
+            throw new RuntimeException(actual + " is not equal to expected "+ esperado);
+        }
 
     }
 }
