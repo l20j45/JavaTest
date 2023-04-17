eb81bcfd373127d38f54222115731d28dadc8259
Daniel Rojas <daniel.rojas.artiaga@gmail.com>
2023-04-17 17:41:37 -0600

    lesson 3, I use JUnit Testing to create 4 tests

--- src/main/java/com/l20j45/StringUtil.java
+++ src/main/java/com/l20j45/StringUtil.java
@@ -3,6 +3,10 @@ package com.l20j45;
 public class StringUtil {
 
     public static String repeat(String str, int times){
+
+        if (times < 0)
+            throw new IllegalArgumentException("negative times not allowed");
+
         String result ="";
         for (int i = 0; i < times; i++) {
             result +=str;
--- src/test/java/com/l20j45/StringUtilTest.java
+++ src/test/java/com/l20j45/StringUtilTest.java
@@ -1,21 +1,33 @@
 package com.l20j45;
 
 
+import org.junit.Assert;
+import org.junit.Test;
 
 public class StringUtilTest {
-    public static void main(String[] args) {
 
-        assertEquals(StringUtil.repeat("hola",3),"holaholahola");
-
-
-        assertEquals(StringUtil.repeat("hola",1),"hola");
-
+    @Test
+    public void testRepeat_oneTime() {
+        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+    }
+    @Test
+    public void testRepeat_zeroTime() {
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+    @Test
+    public void testRepeat_multipleTime() {
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
     }
 
-    private static void assertEquals(String actual, String esperado) {
-        if(!actual.equals(esperado)){
-            throw new RuntimeException(actual + " is not equal to expected "+ esperado);
+    @Test(expected = IllegalArgumentException.class)
+    public void testRepeat_zeroTimes() {
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", -1));
+    }
+
+/*    private static void assertEquals(String actual, String esperado) {
+        if (!actual.equals(esperado)) {
+            throw new RuntimeException(actual + " is not equal to expected " + esperado);
         }
 
-    }
+    }*/
 }
